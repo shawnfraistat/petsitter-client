a299cb9c90897888d1ec2c5aa29a914d0f5159c2
shawnfraistat <shawn.fraistat@gmail.com>
2019-01-17 14:15:43 -0500

    Fix MenuItems So You Don't Have to Click Exactly On Link
    Users now don't have to click exactly on the text to navigate between
    different menu options; clicking on the container is sufficient.

--- src/header/NavMenu.jsx
+++ src/header/NavMenu.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
@@ -46,7 +46,7 @@ class NavMenu extends Component {
 
   render() {
     const { anchorEl } = this.state
-    const { user, classes } = this.props
+    const { user, classes, history } = this.props
 
     console.log('this.props is', this.props)
     const switchToClient = (setUser, getUser) => {
@@ -62,19 +62,19 @@ class NavMenu extends Component {
     }
 
     const authenticatedOptions = [
-      (<MenuItem className={classes.menuItem} key="edit-profile" onClick={this.handleClose}><Link to="/edit-profile">Edit Profile</Link></MenuItem>),
-      (<MenuItem className={classes.menuItem} key="sign-out" onClick={this.handleClose}><Link to="/sign-out">Sign Out</Link></MenuItem>)
+      (<MenuItem className={classes.menuItem} key="edit-profile" onClick={() => history.push('/edit-profile')}><Link to="/edit-profile">Edit Profile</Link></MenuItem>),
+      (<MenuItem className={classes.menuItem} key="sign-out" onClick={() => history.push('/sign-out')}><Link to="/sign-out">Sign Out</Link></MenuItem>)
     ]
 
 
     const unauthenticatedOptions = [
-      (<MenuItem className={classes.menuItem} key="sign-up" onClick={this.handleClose}><Link to="/sign-up">Sign Up</Link></MenuItem>),
-      (<MenuItem className={classes.menuItem} key="sign-in" onClick={this.handleClose}><Link to="/sign-in">Sign In</Link></MenuItem>)
+      (<MenuItem className={classes.menuItem} key="sign-up"onClick={() => history.push('/sign-up')}><Link to="/sign-up">Sign Up</Link></MenuItem>),
+      (<MenuItem className={classes.menuItem} key="sign-in" onClick={() => history.push('/sign-in')}><Link to="/sign-in">Sign In</Link></MenuItem>)
     ]
 
 
     const alwaysOptions = (
-      <MenuItem className={classes.menuItem} onClick={this.handleClose}><Link to="/">Home</Link></MenuItem>
+      <MenuItem className={classes.menuItem} onClick={() => history.push('/')}><Link to="/">Home</Link></MenuItem>
     )
 
     return (
@@ -94,13 +94,14 @@ class NavMenu extends Component {
           open={Boolean(anchorEl)}
           onClose={this.handleClose}
         >
-          { (user && user.isClient) && (<MenuItem className={classes.menuItem} onClick={this.handleClose}><Link to="/client">Search Sitters</Link></MenuItem>)}
+          { (user && user.isClient) && (<MenuItem className={classes.menuItem} onClick={() => history.push('/client')}><Link to="/client">Search Sitters</Link></MenuItem>)}
+          { (user && user.isSitter) && (<MenuItem className={classes.menuItem} onClick={() => history.push('/sitter')}><Link to="/client">Info for Sitters</Link></MenuItem>)}
           {/* if user has both a client and sitter account, let user switch between them */}
-          { (user && user.isSitter && user.hasClientAcc) && (<MenuItem className={classes.menuItem} onClick={this.handleClose}><Link to="/client" onClick={() => switchToClient(setUser, getUser)}>Switch to Client View</Link></MenuItem>) }
-          { (user && user.isClient && user.hasSitterAcc) && (<MenuItem  className={classes.menuItem}onClick={this.handleClose}><Link to="/sitter" onClick={() => switchToSitter(setUser, getUser)}>Switch to Sitter View</Link></MenuItem>) }
+          { (user && user.isSitter && user.hasClientAcc) && (<MenuItem className={classes.menuItem} onClick={() => history.push('/client')}><Link to="/client" onClick={() => switchToClient(setUser, getUser)}>Switch to Client View</Link></MenuItem>) }
+          { (user && user.isClient && user.hasSitterAcc) && (<MenuItem  className={classes.menuItem}  onClick={() => history.push('/sitter')}><Link to="/sitter" onClick={() => switchToSitter(setUser, getUser)}>Switch to Sitter View</Link></MenuItem>) }
           {/* if user doesn't have a client or sitter account, let user create it */}
-          { (user && !(user.hasClientAcc)) && (<MenuItem className={classes.menuItem} onClick={this.handleClose}><Link to="/create-client-account">Create Client Account</Link></MenuItem>) }
-          { (user && !(user.hasSitterAcc)) && (<MenuItem className={classes.menuItem} onClick={this.handleClose}><Link to="/create-sitter-account">Create Sitter Account</Link></MenuItem>) }
+          { (user && !(user.hasClientAcc)) && (<MenuItem className={classes.menuItem}  onClick={() => history.push('/create-client-account')}><Link to="/create-client-account">Create Client Account</Link></MenuItem>) }
+          { (user && !(user.hasSitterAcc)) && (<MenuItem className={classes.menuItem}  onClick={() => history.push('/create-sitter-account')}><Link to="/create-sitter-account">Create Sitter Account</Link></MenuItem>) }
           { user ? authenticatedOptions : unauthenticatedOptions }
           { alwaysOptions }
         </Menu>
@@ -109,4 +110,5 @@ class NavMenu extends Component {
   }
 }
 
-export default withStyles(styles)(NavMenu)
+const NavStyles = withStyles(styles)(NavMenu)
+export default withRouter(NavStyles)
--- src/sitter-view/SitterLanding.jsx
+++ src/sitter-view/SitterLanding.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import './SitterLanding.scss'
 
-class SitterLanding extends Component {
-  render () {
-    return (
-      <h1>You reached the sitter page</h1>
-    )
-  }
+const SitterLanding = () => {
+  return (
+    <div className="landing-div">
+      <div className="man-and-dog"><h1 className="welcome-text">Welcome</h1></div>
+      <div className="first-question-div">
+        <h3 className="about landing-h3">About</h3>
+        <div className="text-div">
+          <p className="landing-page-text"><span className="petsitter-text">petsitter.io</span> was founded by a man with a simple dream, a dream of being able to easily find a pet sitter without having to beg friends for favors.</p>
+          <p className="landing-page-text">So he created this site, which allows pet sitters to post their information and email address, and clients to search for sitters who match their specifications.</p>
+          <p className="landing-page-text">If you want to use this site as a sitter, it&apos;s as simple as signing up and waiting for the e-mails to roll in. As for clients, they can sign up and search away!</p>
+        </div>
+      </div>
+      <div className="row second-question-div">
+        <img className="cat-and-dog" src={require('../images/cat-and-dog.jpg')} />
+        <div className="cat-and-dog-text">
+          <h3 className="cared-for landing-h3">Will My Pets Be Well Cared-For?</h3>
+          <p className="landing-page-text">If this were a real site, absolutely.</p>
+        </div>
+      </div>
+      <div className="third-question-div">
+        <h3 className="landing-h3">Are the Sitters on This Site Verified By &nbsp;<span className="petsitter-text">petsitter.io</span>?</h3>
+        <div className="row verified-div">
+          <div className="verified-text-div">
+            <p className="landing-page-text">Yes.&#42; You can contact a sitter knowing that our qualified team has verified that they will take good care of your pets.</p>
+            <br />
+            <br />
+            &#42; No. This isn&apos;t real. If any of our sitters actually respond to your emails, run.
+          </div>
+          <img className="high-five-kitten" src={require('../images/high-five-kitten.jpg')} />
+        </div>
+      </div>
+    </div>
+  )
 }
+
 
 export default SitterLanding
