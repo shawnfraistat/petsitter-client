252649bc6e0959d1ef52dc314658462b2d205463
shawnfraistat <shawn.fraistat@gmail.com>
2019-02-01 15:48:47 -0500

    Adjust Site Description B/c Users Can Now Message Each Other
    Adjusted the site description to reflect the fact that users are no
    longer emailing each other, but are exchanging messages through the
    site.

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -9,8 +9,8 @@ const LandingPage = () => {
         <h3 className="about landing-h3">About</h3>
         <div className="text-div">
           <p className="landing-page-text"><span className="petsitter-text">petsitter.io</span> was founded by a man with a simple dream, a dream of being able to easily find a pet sitter without having to beg friends for favors.</p>
-          <p className="landing-page-text">So he created this site, which allows pet sitters to post their information and email address, and clients to search for sitters who match their specifications.</p>
-          <p className="landing-page-text">If you want to use this site as a sitter, it&apos;s as simple as signing up and waiting for the e-mails to roll in. As for clients, they can sign up and search away!</p>
+          <p className="landing-page-text">So he created this site, which allows pet sitters to post their information and exchange messages with clients, and clients to search for sitters who match their specifications.</p>
+          <p className="landing-page-text">If you want to use this site as a sitter, it&apos;s as simple as signing up and waiting for the messages to roll in. As for clients, they can sign up and search away!</p>
         </div>
       </div>
       <div className="row second-question-div">
@@ -27,7 +27,7 @@ const LandingPage = () => {
             <p className="landing-page-text">Yes.&#42; You can contact a sitter knowing that our qualified team has verified that they will take good care of your pets.</p>
             <br />
             <br />
-            &#42; No. This isn&apos;t real. If any of our sitters actually respond to your emails, run.
+            &#42; No. This isn&apos;t real. If any of our sitters actually respond to your messages, run.
           </div>
           <img className="high-five-kitten" alt="kitten high-fiving" src={require('./images/high-five-kitten.jpg')} />
         </div>
